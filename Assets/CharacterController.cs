302fb50a9928cff2e3141bd7ca5cfa585160bd95
Javier <javiermarist@gmail.com>
2025-05-19 21:24:25 +0200

    Added Mobile Controllers

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 10f;
+    private Rigidbody2D rb;
+
+    [SerializeField] private GameObject mobileControlsCanvas;
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+    private Vector2 moveInput;
+#endif
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+#if UNITY_ANDROID
+        if (mobileControlsCanvas != null)
+            mobileControlsCanvas.SetActive(true);
+#else
+        if (mobileControlsCanvas != null)
+            mobileControlsCanvas.SetActive(false);
+#endif
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+#if UNITY_EDITOR || UNITY_STANDALONE
+        Vector3 movement = new Vector3(moveInput.x, 0f, 0f);
+        rb.AddForce(movement * speed);
+#endif
     }
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+#endif
 }
